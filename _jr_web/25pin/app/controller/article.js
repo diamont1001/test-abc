ff08a437ec11196f4c67b5076acca08e5e49128e
材主 <jingren.cjr@alibaba-inc.com>
2019-01-05 16:03:39 +0800

    feat: 文章列表tag页title优化

--- _jr_web/25pin/app/controller/article.js
+++ _jr_web/25pin/app/controller/article.js
@@ -99,14 +99,16 @@ class ArticleController extends Controller {
     }
 
     let canonical = this.app.config.biz.server + '/article';
+    let title = '超级有趣冷知识|生活小常识|最新精彩文章';
 
     if (tag) {
       canonical += '/t_' + Xor.encode(tag)
+      title = `「${tag}」相关资讯|最新文章推荐`;
     }
 
     await this.ctx.layoutRender('pages/articlelist/index.ejs', {
       name: 'articlelist',
-      title: '超级有趣冷知识|生活小常识|最新精彩文章',
+      title,
       keywords: '文章,资讯,教程,生活小常识,有趣冷知识,爱玩品',
       description: '最新精彩文章推荐，快来看看网友们都在说些什么吧，点击有惊喜喔。爱玩品资源站，为你推荐丰富好玩的资源，让优质资源脱颖而出',
       canonical,
