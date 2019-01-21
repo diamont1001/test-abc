1371c62844e18aa2a3311d995af3f190e53c2b7f
材主 <jingren.cjr@alibaba-inc.com>
2019-01-21 17:37:34 +0800

    feat: 重新上线 25pin

--- _jr_web/25pin/app/controller/home.js
+++ _jr_web/25pin/app/controller/home.js
@@ -26,8 +26,8 @@ class HomeController extends Controller {
     }
 
     // 备案时用
-    await this.ctx.render('pages/beian/index.ejs');
-    return;
+    // await this.ctx.render('pages/beian/index.ejs');
+    // return;
 
     await this.ctx.layoutRender('pages/home/index.ejs', {
       name: 'home',
