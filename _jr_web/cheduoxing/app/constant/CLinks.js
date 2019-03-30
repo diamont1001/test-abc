a92b5a9aa90cc70a986ff1ba41885c44aa3a3c41
材主 <jingren.cjr@alibaba-inc.com>
2019-03-30 19:34:42 +0800

    feat: cheduoxing 友链

--- _jr_web/cheduoxing/app/constant/CLinks.js
+++ _jr_web/cheduoxing/app/constant/CLinks.js
@@ -5,8 +5,8 @@
 'use strict';
 
 module.exports = [
-  { name: '爱玩品资源站', url: 'http://www.25pin.com' },
-  { name: 'APP下载', url: 'http://www.25pin.com/app' },
-  { name: '冷知识', url: 'http://www.25pin.com/article' },
+  { name: '瓶子老师学习网', url: 'http://www.25pin.com' },
+  { name: '冷知识', url: 'http://www.25pin.com/article/t_1' },
+  { name: '母婴', url: 'http://www.25pin.com/article/t_12' },
   { name: '美女写真', url: 'http://www.25pin.com/photo' },
 ];
