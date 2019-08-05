22d96ecbbced9aa532dfd87ad33f3547f36482df
材主 <diamont1001@163.com>
2019-08-05 18:05:02 +0800

    feat: 添加阿里云友链

--- _jr_web/25pin/app/constant/CLinks.js
+++ _jr_web/25pin/app/constant/CLinks.js
@@ -6,5 +6,7 @@
 
 module.exports = [
   { name: '用车养车', url: 'http://www.cheduoxing.com' },
+  { name: '阿里云', url: 'https://www.aliyun.com/?source=5176.11533457&userCode=hotrg136' },
+  { name: '云栖社区', url: 'https://yq.aliyun.com/?source=5176.11533457&userCode=hotrg136' },
   // { name: '美女写真', url: 'http://www.25pin.com/photo' },
 ];
