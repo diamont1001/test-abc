6d3d0e101c2162d26eb10c0fadf7e8be6a2b4e3b
材主 <diamont1001@163.com>
2019-08-06 19:14:24 +0800

    feat: cheduoxing 添加阿里云友链

--- _jr_web/cheduoxing/app/constant/CLinks.js
+++ _jr_web/cheduoxing/app/constant/CLinks.js
@@ -8,4 +8,6 @@ module.exports = [
   { name: '瓶子老师学习网', url: 'http://www.25pin.com' },
   { name: '冷知识', url: 'http://www.25pin.com/article/t_1' },
   { name: '母婴备孕', url: 'http://www.25pin.com/article/t_12' },
+  { name: '阿里云', url: 'https://www.aliyun.com/?source=5176.11533457&userCode=hotrg136' },
+  { name: '云栖社区', url: 'https://yq.aliyun.com/?source=5176.11533457&userCode=hotrg136' },
 ];
