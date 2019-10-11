94678a899348fb5bfb862ad7b61543acc74f6cc4
diamont1001 <diamont1001@163.com>
2019-10-11 18:41:34 +0800

    feat: 25pin 贪吃蛇小游戏页面适配端内环境
    1）贪吃蛇小游戏页面适配端内环境
    2）layout 在端内环境去掉 CNZZ 引入

--- _jr_web/25pin/app/controller/onlinegame.js
+++ _jr_web/25pin/app/controller/onlinegame.js
@@ -15,6 +15,7 @@ class OnlineGameController extends Controller {
       title: '贪吃蛇，在线怀旧经典小游戏_瓶子老师学习网',
       description: '贪吃蛇，由瓶子老师学习网提供的在线小游戏，这些属于80后一代人的经典手机小游戏，你敢接受挑战吗？点击有惊喜喔喵。',
       canonical: this.app.config.biz.server + '/onlinegame/snake',
+      deviceType: this.ctx.deviceType,
     });
   }
 }
