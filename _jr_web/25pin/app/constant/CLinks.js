e8ab7db8478ac196c14e8c961566494bffefd78c
diamont1001 <diamont1001@163.com>
2021-01-02 17:29:47 -0600

    feat: 增加 mxbbs.ca 友链

--- _jr_web/25pin/app/constant/CLinks.js
+++ _jr_web/25pin/app/constant/CLinks.js
@@ -8,5 +8,6 @@ module.exports = [
   { name: '用车养车', url: 'http://www.cheduoxing.com' },
   { name: '阿里云', url: 'https://www.aliyun.com/?source=5176.11533457&userCode=hotrg136' },
   { name: '云栖社区', url: 'https://yq.aliyun.com/?source=5176.11533457&userCode=hotrg136' },
+  { name: '加拿大华人论坛', url: 'http://www.mxbbs.ca/' },
   // { name: '美女写真', url: 'http://www.25pin.com/photo' },
 ];
